57a4257ae1de5a9b71b44b0ca48ef4fcdc97e249
Nacho <nacho@nachomerino.com>
2018-05-12 13:26:22 +0200

    navabar

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -2,76 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
-import Tabs, { Tab } from 'material-ui/Tabs';
-import Typography from 'material-ui/Typography';
-//
 import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
-// Components
-import Selector from '../selector';
 
-function TabContainer(props) {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
   return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {props.children}
-    </Typography>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            Title
+          </Typography>
+          {props.categories.map( categorie => <Typography key={categorie.id} variant="title" color="inherit" className={classes.flex} > {categorie.name} </Typography>)}
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-  title: {
-    padding:'0 24px 0 0'
-  },
-});
-
-class NavBar extends React.Component {
-  state = {
-      value: 0,
-      reload: false
-  };
-
-  handleChange = (event, value) => {
-    this.setState(prevState => { return {reload: !prevState.reload}})
-    this.setState({ value });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
-    console.log('render navbar', this.props)
-    return (
-
-      <div className={classes.root}>
-        <AppBar position="static">
-        <Toolbar>
-          <Typography variant="title" color="inherit" className={classes.title}>
-            ICON HERE
-          </Typography>
-          <Tabs value={value} onChange={this.handleChange}>
-            {this.props.categories.map( categorie => <Tab onClick={this.handleChange} key={categorie.id} label={categorie.name} />)}
-          </Tabs>
-        </Toolbar> 
-        </AppBar>
-        {value === 0 && <TabContainer><Selector categorie={'Cooking'}/></TabContainer>}
-        {value === 1 && <TabContainer><Selector categorie={'Patterns'}/></TabContainer>}
-        {value === 2 && <TabContainer>...Work in progress</TabContainer>}
-      </div>
-    );
-  }
-}
-
-NavBar.propTypes = {
+ButtonAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(NavBar);
+export default withStyles(styles)(ButtonAppBar);
+
